c1a3042c2b45dad6cab6399cf2e7a85ebfb3df43
valentinavila1 <vaale.avila@hotmail.com>
2023-09-12 23:11:43 -0300

    Ejercicios JS

---
+++ index.js
@@ -0,0 +1,85 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+  },
+
+  {
+    id: 3,
+    nombre: "pizza Napolitana",
+    precio: 1350,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+  },
+
+  {
+    id: 4,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+  },
+
+  {
+    id: 5,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+  },
+
+  {
+    id: 6,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+  },
+];
+
+// a)  Las pizzas que tengan un id impar.
+
+/* const pizzaId = (ArrayPizza) => {
+  ArrayPizza.forEach((id) => {
+    if (id.id % 2 === 1) {
+      return console.log(`La ${id.nombre} tiene el ID ${id.id} y es Impar`);
+    }
+  });
+};
+pizzaId(pizzas); */
+
+//b) Responder: ¿Hay alguna pizza que valga menos de $600?
+
+/* const MenorA600 = pizzas.find((elemento) => elemento.precio < 600);
+console.log(MenorA600.nombre); */
+
+// c) El nombre de cada pizza con su respectivo precio.
+/* const NombreyPrecio = (pizzasArray) => {
+  pizzasArray.forEach((element) => {
+    console.log(`La ${element.nombre} vale $${element.precio}`);
+  });
+};
+NombreyPrecio(pizzas); */
+
+// d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo). Ayuda: van a tener que realizar dos recorridos, ya que cada pizza del array de pizzas tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
+/* const Ingredientes = (pizzasArray) => {
+  pizzasArray.forEach((element) => {
+    console.log(
+      `La ${
+        element.nombre
+      } tiene éstos ingredientes: ${element.ingredientes.join(", ")}`
+    );
+  });
+};
+Ingredientes(pizzas); */
--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
 
-/* const number = 10;
-for (let i = 1; i <= number; i++) {
-  if (i % 2 === 0) {
-    console.log(`${i} es Par`);
+/* function ParImpar(numero) {
+  if (numero % 2 === 0) {
+    console.log(`${numero} es Par`);
   } else {
-    console.log(`${i} es Impar`);
+    console.log(`${numero} es Impar`);
   }
-} */
+}
+ParImpar(13); */
 
 // 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
 /* function Comparativa(num1, num2) {
@@ -31,20 +31,24 @@ Comparativa(2, 10); */
 multiplode5(20); */
 
 //4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
-
-/* const Numero = 10;
-for (i = 0; i <= Numero; i++) {
-  console.log(i);
-} */
+/* function PrintingNumbers(Numero) {
+  for (i = 0; i <= Numero; i++) {
+    console.log(i);
+  }
+}
+PrintingNumbers(20); */
 
 //5 ----(Falta éste) Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
 
 //6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
 
-/* let Array = [1, 2, 3, 4, 5, 6, 7];
-for (i = 0; i < Array.length; i++) {
-  console.log(Array[i]);
-} */
+/* arr = [1, 2, 3, 4, 5];
+function imprimirArray(array) {
+  for (i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+imprimirArray(arr); */
 
 //7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 
@@ -62,9 +66,66 @@ continuar5(Numbers); */
 
 //8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
 /* const Numbers = [1, 2, 3, 4, 5];
-function MultiplicarNumero(num) {
-  for (i = 0; i < Numbers.length; i++) {
-    console.log(`${num} x ${Numbers[i]} = ${num * Numbers[i]}`);
+function MultiplicarNumero(num, array) {
+  for (i = 0; i < array.length; i++) {
+    console.log(`${num} x ${array[i]} = ${num * array[i]}`);
   }
 }
-MultiplicarNumero(22); */
+MultiplicarNumero(2, [1, 2, 3, 4, 5, 6, 7]); */
+
+// ======================================================================================================================================================= //
+// Uso del ForEach (No retorna nada, ejecuta acciones nada mas)
+// const Numbers = [1, 2, 3, 4, 5];
+// Numbers.forEach((numero) => console.log(numero));
+// -----------------------------------
+
+// Uso edl map (Retorna un nuevo Array)
+/* const Numbers = [1, 2, 3, 4, 5];
+const duplicados = Numbers.map((Numero) => Numero * 2);
+console.log(Numbers);
+console.log(duplicados); */
+// ----------------------------------
+
+/* REDUCE (0 + NUMERO ACTUALIZADO. ACUMULADOR = 0 Y VA ITERANDO)
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+const sumaArray = numeros.reduce((acumulador, valoractualizado) => {
+  return acumulador + valoractualizado;
+});
+console.log(sumaArray); */
+
+// const Helados = ["ddl", "vainilla", "chocolate", "americana", "tramontana"];
+/* function CambiarHelado(gusto) {
+  Helados.push(Helados.shift());
+  console.log(Helados);
+}
+CambiarHelado(); */
+
+/* function InvertirHelados(gusto) {
+  console.log(Helados.reverse().join("-"));
+}
+InvertirHelados(); */
+
+/* const Helados2 = ["Banana Split", "Naranja", "Menta"];
+const UnirGustos = () => {
+  let gustos = Helados.slice(3, 5).concat(Helados2.slice(0, 2));
+  return gustos;
+};
+console.log(UnirGustos()); */
+
+// ----------
+/*
+let text =
+  "         .levin us ed sojel yum atse isseM y odnum led rodaguj rojem le se odlanoR          ";
+
+const ModificatedText = () => {
+  text = text
+    .trim()
+    .split("")
+    .reverse("")
+    .join("")
+    .replace("Ronaldo", "Messi".replace("Messi", "Ronaldo"));
+
+  console.log(text);
+};
+
+ModificatedText(); */
